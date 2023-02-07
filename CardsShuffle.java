1d2ffca6ab9022b0dd00fe42b58a9eb664df3593
Nilesh Pawar <119921719+npawar025@users.noreply.github.com>
2023-02-07 09:15:25 +0530

    Update CardsShuffle.java

--- CardsShuffle.java
+++ CardsShuffle.java
@@ -39,9 +39,34 @@ Sample Output 3
 2
 */
 
-//Both solutions are giving me MLE - used buffereader
 
 import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n+1];
+		
+		for(int i=1; i<4*n; i++) {
+			arr[Integer.parseInt(st.nextToken())]++;
+		}
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i] == 3) System.out.println(i);
+		}
+		
+ 	}
+}
+
+//Both solutions are giving me MLE - used buffereader
+
+/*import java.io.*; // for handling input/output
 import java.util.*; // contains Collections framework
 
 // don't change the name of this class
@@ -58,6 +83,7 @@ class Main {
         System.out.println(ans);
     }
 }
+*/
 
 /*
 class Main {
